daf343ec2f4c851ed779768c5f03c9f091e5a913
MacRat <m@crat.jp>
2020-02-29 22:33:38 +0900

    Added page loading animation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,38 +1,81 @@
+import {useState, useEffect} from 'react';
 import {AppProps} from 'next/app';
+import Router from 'next/router';
 
 import Header from '../components/Header';
 import MDXComponents from '../components/MDXComponents';
 
 
-const TomboApp = ({Component, pageProps}: AppProps) => (
-    <main>
-        <Header />
+const TomboApp = ({Component, pageProps}: AppProps) => {
+    const [loading, setLoading] = useState<boolean>(false);
 
-        <MDXComponents>
-            <Component {...pageProps} />
-        </MDXComponents>
+    useEffect(() => {
+        const onStart = () => setLoading(true);
+        const onComplete = () => setLoading(false);
 
-        <style jsx global>{`
-            html {
-                background-color: #eee;
-                color: #322;
-                font-family: 'Noto Sans JP', gothic, sans-serif;
-                overflow: hidden auto;
-            }
-            body {
-                margin: 0;
-            }
-            a {
-                position: relative;
-                color: #63f;
-                transition: color .2s ease;
-            }
-            a:hover, a:focus {
-                color: #f36;
-            }
-        `}</style>
-    </main>
-);
+        Router.events.on('routeChangeStart', onStart);
+        Router.events.on('routeChangeComplete', onComplete);
+
+        return () => {
+            Router.events.off('routeChangeStart', onStart);
+            Router.events.off('routeChangeComplete', onComplete);
+        };
+    }, []);
+
+    return (
+        <main className={loading ? "loading" : ""}>
+            <Header />
+
+            <MDXComponents>
+                <Component {...pageProps} />
+            </MDXComponents>
+
+            <style jsx global>{`
+                html {
+                    background-color: #eee;
+                    color: #322;
+                    font-family: 'Noto Sans JP', gothic, sans-serif;
+                    overflow: hidden auto;
+                }
+                body {
+                    margin: 0;
+                }
+                a {
+                    position: relative;
+                    color: #63f;
+                    transition: color .2s ease;
+                }
+                a:hover, a:focus {
+                    color: #f36;
+                }
+            `}</style>
+
+            <style jsx>{`
+                main::after {
+                    content: '';
+                    display: block;
+                    position: fixed;
+                    top: 0;
+                    height: .3mm;
+                    background-color: #322;
+                    animation: done .5s ease both;
+                }
+                @keyframes done {
+                    from { left: 0; width: 100%; }
+                      to { left: 100%; width: 0; }
+                }
+                main.loading::after  {
+                    animation: loading 1s linear infinite;
+                }
+                @keyframes loading {
+                      0% { left:    0; width: 0; }
+                     50% { left:    0; width: 100%; }
+                    100% { left: 100%; width: 0; }
+                }
+            `}</style>
+        </main>
+    );
+};
 
 
 export default TomboApp;
