b95613e15f0c78656d55516a847966f1b1db9979
MacRat <m@crat.jp>
2020-02-29 23:47:57 +0900

    Removed tombo design page because it's outdated

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,6 @@ const Index = () => (
         <ul>
             <li><Link href="/backdrop"><a>backdrop</a></Link></li>
             <li><Link href="/polygon"><a>polygon</a></Link></li>
-            <li><Link href="/tombo"><a>tombo</a></Link></li>
             <li><Link href="/blog"><a>blog</a></Link></li>
         </ul>
     </Article>
