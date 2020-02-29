5b4755c1df46b729f2ab1e611235365c8f858911
MacRat <m@crat.jp>
2020-02-29 16:13:42 +0900

    Enhanced header of tombo design

--- pages/tombo.tsx
+++ pages/tombo.tsx
@@ -1,6 +1,94 @@
 import {FC} from 'react';
 
 
+const Header: FC<{}> = () => (
+    <header>
+        <h1>Blanktar</h1>
+        <nav>
+            <ul>
+                <li><a href="">blog</a></li>
+                <li><a href="">works</a></li>
+                <li><a href="">photos</a></li>
+                <li><a href="">about</a></li>
+            </ul>
+        </nav>
+
+        <style jsx>{`
+            header {
+                text-align: center;
+            }
+            header h1 {
+                font-size: 8mm;
+                font-weight: inherit;
+                margin: 0;
+            }
+            header h1::after {
+                content: '';
+                display: block;
+                height: 0.4mm;
+                width: 5cm;
+                position: relative;
+                left: calc(50% - 2.5cm);
+                background-color: black;
+            }
+
+            ul {
+                margin: 0;
+                padding: 0;
+            }
+            li {
+                display: inline-block;
+                margin: 2px 8px;
+                position: relative;
+                animation: menu-fg .2s ease both;
+            }
+            @keyframes menu-fg {
+                     0% { color: transparent; }
+                    50% { color: transparent; }
+                50.001% { color: inherit; }
+            }
+            li::before {
+                content: '';
+                display: block;
+                position: absolute;
+                left: 0;
+                width: 100%;
+                top: 100%;
+                height: 0;
+                background-color: black;
+                animation: menu-bg .3s ease;
+                transition: top .2s ease, height .2s ease;
+            }
+            @keyframes menu-bg {
+                0% { top: 0; height: 0; }
+                50% { top: 0; height: 100%; }
+                100% { top: 100%; height: 0; }
+            }
+            li:nth-child(2)::before { animation-delay: .1s; }
+            li:nth-child(3)::before { animation-delay: .2s; }
+            li:nth-child(4)::before { animation-delay: .3s; }
+
+            a {
+                position: relative;
+                display: inline-block;
+                padding: 2px 8px;
+                z-index: 1;
+                color: inherit;
+                text-decoration: none;
+                transition: color .2s ease;
+            }
+            li:hover a {
+                color: white;
+            }
+            li:hover::before {
+                top: 0;
+                height: 100%;
+            }
+        `}</style>
+    </header>
+);
+
+
 const TomboArticle: FC<{color?: string}> = ({children, color='black'}) => (
     <div>
         <svg width="16mm" height="16mm" viewBox="-0.5 -0.5 16 16" className="tombo top left">
@@ -87,9 +175,7 @@ const TomboArticle: FC<{color?: string}> = ({children, color='black'}) => (
 
 const Tombo = () => (
     <>
-        <header>
-            Blanktar
-        </header>
+        <Header />
 
         <TomboArticle>
             <h1>this is a content</h1>
@@ -104,26 +190,6 @@ const Tombo = () => (
                 <p>section!!</p>
             </section>
         </TomboArticle>
-
-        <style jsx>{`
-            header {
-                font-size: 8mm;
-                text-align: center;
-            }
-            header::after {
-                content: '';
-                display: block;
-                height: 0.4mm;
-                position: relative;
-                left: calc(50% - 2.5cm);
-                background-color: black;
-                animation: header-draw .6s ease both;
-            }
-            @keyframes header-draw {
-                from { width: 0; }
-                  to { width: 5cm; }
-            }
-        `}</style>
     </>
 );
 
