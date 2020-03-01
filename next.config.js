e9a2972aa9da61668b26e5c9398f25171ea71593
MacRat <m@crat.jp>
2020-03-01 22:59:44 +0900

    Added bundle-analyzer

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+    enabled: process.env.ANALYZE === 'true',
+});
+
+
 const withMdxEnhanced = require('next-mdx-enhanced')({
-    fileExtensions: ['mdx'],
     defaultLayout: true,
 });
 
 
-module.exports = withMdxEnhanced({
+module.exports = withBundleAnalyzer(withMdxEnhanced({
     pageExtensions: ['ts', 'tsx', 'mdx'],
-});
+}));
