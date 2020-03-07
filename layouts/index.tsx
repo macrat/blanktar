02bd2e5a18c44ae4a6774dbb915b387c421b9578
MacRat <m@crat.jp>
2020-03-07 16:58:30 +0900

    Enhanced frontmatter validator and fixed wrong articles

--- layouts/index.tsx
+++ layouts/index.tsx
@@ -10,23 +10,27 @@ import ComponentsProvider from './components';
 export type Props = {
     title: string,
     pubtime: string,
+    amp: boolean | 'hybrid',
     tags: string[],
     image?: string,
-    description: string,
+    description: string | null,
 };
 
 
-export default ({title, pubtime, tags, image, description}: Props) => {
+export default ({title, pubtime, amp, tags, image, description}: Props) => {
     if (!title) {
         throw `${pubtime}: title is not provided`;
     }
     if (!pubtime || !pubtime.match(/^20[0-9]{2}-(0[0-9]|1[0-2])-([0-2][0-9]|3[0-1])T([0-1][0-9]|2[0-3]):[0-5][0-9]\+0900$/)) {
         throw `${title}: pubtime is not provided or invalid format: "${pubtime}"`;
     }
+    if (![true, false, 'hybrid'].includes(amp)) {
+        throw `${title} ${pubtime}: amp is not provided or invalid value: "${amp}"`;
+    }
     if (!tags || tags.length === 0) {
         throw `${title} ${pubtime}: tags is not provided`;
     }
-    if (!description) {
+    if (!description && description !== null) {
         throw `${title} ${pubtime}: description is not provided`;
     }
 
