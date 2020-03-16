e3e99292011b5a3ce1346018193cae0a0619f4e8
MacRat <m@crat.jp>
2020-03-16 22:26:29 +0900

    Replaced SVG loader by preval.macro
    See #27

--- pages/api/eyecatch/[title].ts
+++ pages/api/eyecatch/[title].ts
@@ -1,16 +1,31 @@
 import {NextApiRequest, NextApiResponse} from 'next';
-import {createCanvas, registerFont, loadImage} from 'canvas';
+import {createCanvas, registerFont, Image} from 'canvas';
+import preval from 'preval.macro';
 
 
 registerFont('./assets/NotoSansCJKjp-Light.otf', {family: 'NotoSansJP', weight: 'Light'});
+
+
+const baseImage = () => {
+    return new Promise((resolve, reject) => {
+        const img = new Image();
+
+        img.onload = () => resolve(img);
+        img.onerror = err => reject(err);
+
+        img.src = preval`
+            const fs = require('fs');
+            module.exports = 'data:image/svg+xml;base64,' + fs.readFileSync('./assets/eyecatch-base.svg', 'base64');
+        `;
+    });
+};
 
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
     const canvas = createCanvas(1200, 1200);
     const ctx = canvas.getContext('2d');
 
-    const base = await loadImage('./assets/eyecatch-base.svg');
-    ctx.drawImage(base, 0, 0, 1200, 1200);
+    ctx.drawImage(await baseImage(), 0, 0, 1200, 1200);
 
     ctx.font = 'Light 72px NotoSansJP';
     ctx.textAlign = 'center';
