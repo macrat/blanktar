79d21dad340fe959d973fb183b2b3acfcbcb5fcc
MacRat <m@crat.jp>
2020-03-21 15:08:10 +0900

    Made dynamic load posts in development build for hot reloading blog
    Closed #47

--- lib/posts/index.ts
+++ lib/posts/index.ts
@@ -1,10 +1,12 @@
 import preval from 'preval.macro';
+import penv from 'penv.macro';
 
 import {Post as P} from './types';
 
 
 export type Post = P;
 
-export default (preval`module.exports = require('./loader')` as Post[]).map(x => ({
-    ...x,
-}));
+export default penv(
+    {development: require('./loader') as Post[]},
+    preval`module.exports = require('./loader')` as Post[],
+);
