4d5d28c187ee8cef540b6fce901c84f87bebd210
MacRat <m@crat.jp>
2020-03-25 22:47:44 +0900

    Fixed CI fail by timezone

--- components/__tests__/DateTime.js
+++ components/__tests__/DateTime.js
@@ -2,10 +2,10 @@ import {date2str, date2readable} from '../DateTime';
 
 
 test('date2str', () => {
-    expect(date2str(new Date('2001-01-02T03:04:05+0900'))).toBe('2001/01/02 3:04');
+    expect(date2str(new Date('2001-01-02T03:04:05'))).toBe('2001/01/02 3:04');
 });
 
 
 test('date2readable', () => {
-    expect(date2readable(new Date('2001-01-02T03:04:05+0900'))).toBe('2001年1月2日 3時4分の記事');
+    expect(date2readable(new Date('2001-01-02T03:04:05'))).toBe('2001年1月2日 3時4分の記事');
 });
