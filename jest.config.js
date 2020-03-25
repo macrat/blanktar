c7ce4e0f02aed2dde79e52947d9f4b4c434bf49f
MacRat <m@crat.jp>
2020-03-25 23:02:51 +0900

    Added path alias for jest test

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,9 @@
 module.exports = {
     setupFiles: ['./jest.setup.ts'],
     testMatch: ['**/__tests__/**/*.js?(x)'],
+    moduleNameMapper: {
+        '~/(.+)': '<rootDir>/$1',
+    },
     testPathIgnorePatterns: [
         '/.git/',
         '/.mdx-data/',
