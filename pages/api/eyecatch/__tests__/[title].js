9befa7c9a3ef72f69e77d9cd80c0c89664c1bd24
MacRat <m@crat.jp>
2020-03-28 18:31:48 +0900

    Added benchmark of eyecatch image

--- pages/api/eyecatch/__tests__/[title].js
+++ pages/api/eyecatch/__tests__/[title].js
@@ -1,4 +1,28 @@
+import Benchmark from 'asyncmark';
+
 import eyecatch from '../[title]';
+
+
+const execute = async (title) => {
+    const headers = {};
+
+    await eyecatch({
+        headers: {},
+        query: {
+            title: title,
+        },
+    }, {
+        setHeader(name, value) {
+            headers[name] = value;
+            return this;
+        },
+        send(data) {
+            return this;
+        },
+    });
+
+    return {headers};
+}
 
 
 describe("don't crash", () => {
@@ -9,24 +33,22 @@ describe("don't crash", () => {
         ['long japanese', 'テスト '.repeat(30)],
     ].forEach(([name, title]) => {
         test(name, async () => {
-            const headers = {};
-
-            await eyecatch({
-                headers: {},
-                query: {
-                    title: title,
-                },
-            }, {
-                setHeader(name, value) {
-                    headers[name] = value;
-                    return this;
-                },
-                send(data) {
-                    return this;
-                },
-            });
+            const {headers} = await execute(title);
 
             expect(headers['Content-Type']).toBe('image/png');
         });
     });
+});
+
+
+test('benchmark', async () => {
+    const result = await new Benchmark({
+        name: 'eyecatch',
+        number: 5,
+        async fun() {
+            await execute('this is a test');
+        },
+    }).run();
+
+    result.assert('<200ms');
 });
