08b5d331bf10bf8c16e6c9d3151f573096ebbe19
MacRat <m@crat.jp>
2020-04-03 19:08:33 +0900

    Removed rarely language supports on syntax highlight

--- components/HighlightCode/language-supports.js
+++ components/HighlightCode/language-supports.js
@@ -2,8 +2,4 @@ import Prism from 'prism-react-renderer/prism';
 
 (typeof global !== 'undefined' ? global : window).Prism = Prism;
 
-require('prismjs/components/prism-csharp');
-require('prismjs/components/prism-docker');
-require('prismjs/components/prism-r');
-require('prismjs/components/prism-swift');
 require('prismjs/components/prism-toml');
