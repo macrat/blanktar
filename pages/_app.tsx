eb88a7b163cb5a29d89b16ab51312dddd0b87f4d
MacRat <m@crat.jp>
2020-04-12 00:37:03 +0900

    Splitted CommonResources component from _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,67 +1,13 @@
-import React, {FC, useState, useEffect, memo} from 'react';
+import React, {FC, useEffect} from 'react';
 import {AppProps} from 'next/app';
-import Head from 'next/head';
-import {useAmp} from 'next/amp';
 
 import useLoading from '~/lib/loading';
 import useAnalytics from '~/lib/analytics';
 
+import CommonResources from '~/components/CommonResources';
 import Header from '~/components/Header';
-import JsonLD, {Website} from '~/components/JsonLD';
 import SearchBar from '~/components/SearchBar';
 import Footer from '~/components/Footer';
-
-
-const CommonResources = memo(function CommonResources() {
-    const [fontCSS, setFontCSS] = useState<string>("");
-    const isAmp = useAmp();
-
-    useEffect(() => {
-        fetch('/font.css')
-            .then(resp => resp.text())
-            .then(css => setFontCSS(URL.createObjectURL(new Blob([css], {type: 'text/css'}))));
-    }, []);
-
-    return (
-        <Head>
-            <meta charSet="utf-8" />
-
-            <link
-                rel="preconnect"
-                href="https://fonts.gstatic.com"
-                crossOrigin="anonymous"
-                key="preconnect--gstatic" />
-            {isAmp ? '' : (
-                <link
-                    rel="prefetch"
-                    as="stylesheet"
-                    type="text/css"
-                    href="/font.css"
-                    key="prefetch--font" />
-            )}
-            <link
-                rel="preconnect"
-                href="https://www.google-analytics.com"
-                crossOrigin="anonymous"
-                key="preconnect--google-analytics" />
-            <link
-                rel="stylesheet"
-                type="text/css"
-                href={isAmp ? 'https://fonts.googleapis.com/css?family=Noto+Sans+JP:100,300,400&display=swap&subset=japanese' : fontCSS}
-                crossOrigin={isAmp ? "anonymous" : undefined}
-                key="style--font" />
-
-            <meta name="theme-color" content="#402020" />
-            <link rel="icon" sizes="any" type="image/svg+xml" href="/favicon.svg" key="favicon--svg" />
-            <link rel="icon" sizes="512x512" type="image/png" href="/img/blanktar-logo.png" key="favicon--png-512x512" />
-            <link rel="mask-icon" type="image/svg+xml" href="/mask-icon.svg" color="#402020" key="favicon--mask" />
-
-            <link rel="alternate" type="application/atom+xml" href="/blog/feed.xml" key="feed" />
-
-            <JsonLD data={Website} />
-        </Head>
-    );
-} as FC<{}>, () => true);
 
 
 const BlanktarApp = ({Component, pageProps}: AppProps) => {
