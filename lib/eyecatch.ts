483c1c6adfc873d6f6c0b46efae383075a842298
MacRat <m@crat.jp>
2020-05-14 20:50:26 +0900

    Supported custom eyecatch for Pinterest share button

--- lib/eyecatch.ts
+++ lib/eyecatch.ts
@@ -1,6 +1,8 @@
 export function getImageURL(title: string, image?: string): string {
-    if (image) {
+    if (typeof image === 'string') {
         return `https://blanktar.jp${image}`;
+    } else if (image) {
+        return `https://blanktar.jp${image[0]}`;
     }
     if (title) {
         return `https://blanktar.jp/img/eyecatch/1x1/${encodeURIComponent(title)}.png`;
