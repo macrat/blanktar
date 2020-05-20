366857aa0c3c941d596e24fc0754454f10199d12
MacRat <m@crat.jp>
2020-05-20 19:44:53 +0900

    Added test of Image.download
    Closed #186

--- lib/__tests__/image.ts
+++ lib/__tests__/image.ts
@@ -1,5 +1,6 @@
 import {promises as fs} from 'fs';
 import Benchmark from 'asyncmark';
+import fetchMock from 'jest-fetch-mock';
 
 import Image from '../image';
 
@@ -10,6 +11,16 @@ beforeEach(async () => {
 
 
 describe('detect image size', () => {
+    beforeEach(async () => {
+        fetchMock.enableMocks();
+
+        fetchMock.mockResponse(await fs.readFile('./assets/eyecatch-base-4x3.svg'));
+    });
+
+    afterEach(() => {
+        fetchMock.disableMocks();
+    });
+
     test('1024x1024.png', async () => {
         const {width, height} = await (new Image('public/img/blanktar-logo@1024.png')).size();
 
@@ -19,6 +30,13 @@ describe('detect image size', () => {
 
     test('312x60.png', async () => {
         const {width, height} = await (new Image('public/img/blanktar-banner.png')).size();
+
+        expect(width).toBe(312);
+        expect(height).toBe(60);
+    });
+
+    test('from online', async () => {
+        const {width, height} = await (await Image.download('http://example.com/foo/bar.svg')).size();
 
         expect(width).toBe(312);
         expect(height).toBe(60);
