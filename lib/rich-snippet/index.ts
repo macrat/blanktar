530885c4907a8cb097c2d7f443bb207e78086a36
MacRat <m@crat.jp>
2020-06-06 15:49:47 +0900

    Fixed eslint annotation style

--- lib/rich-snippet/index.ts
+++ lib/rich-snippet/index.ts
@@ -7,7 +7,7 @@ type Snippets = {
 };
 
 
-// eslint-disable @typescript-eslint/no-var-requires
+/* eslint-disable @typescript-eslint/no-var-requires */
 const snippets: Snippets = penv(
     {
         development: require('./loader') as Snippets,
@@ -15,7 +15,7 @@ const snippets: Snippets = penv(
     },
     preval`module.exports = require('./loader')` as Snippets,
 );
-// eslint-enable @typescript-eslint/no-var-requires
+/* eslint-enable @typescript-eslint/no-var-requires */
 
 
 export default (query: string): string | undefined => {
