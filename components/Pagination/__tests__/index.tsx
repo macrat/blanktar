3d8a676363a8af2395b9fc504cb05dcbdd01e6a1
MacRat <m@crat.jp>
2020-07-12 23:46:51 +0900

    Added Pagination test

---
+++ components/Pagination/__tests__/index.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import Pagination from '..';
+
+
+describe('Pagination', () => {
+    test('first of many pages', () => {
+        const pagination = shallow(
+            <Pagination current={1} total={10} href={(page: number) => '/'} />
+        ).render();
+
+        expect(pagination.find('.prev').length).toBe(0);
+        expect(pagination.find('.next').length).toBe(1);
+
+        expect(pagination.text()).toBe('1234567次へ');
+        expect(pagination.find('.current').text()).toBe('11');
+    });
+
+    test('first of less pages', () => {
+        const pagination = shallow(
+            <Pagination current={1} total={5} href={(page: number) => '/'} />
+        ).render();
+
+        expect(pagination.find('.prev').length).toBe(0);
+        expect(pagination.find('.next').length).toBe(1);
+
+        expect(pagination.text()).toBe('12345次へ');
+        expect(pagination.find('.current').text()).toBe('11');
+    });
+
+    test('last of many pages', () => {
+        const pagination = shallow(
+            <Pagination current={10} total={10} href={(page: number) => '/'} />
+        ).render();
+
+        expect(pagination.find('.prev').length).toBe(1);
+        expect(pagination.find('.next').length).toBe(0);
+
+        expect(pagination.text()).toBe('前へ45678910');
+        expect(pagination.find('.current').text()).toBe('1010');
+    });
+
+    test('last of less pages', () => {
+        const pagination = shallow(
+            <Pagination current={5} total={5} href={(page: number) => '/'} />
+        ).render();
+
+        expect(pagination.find('.prev').length).toBe(1);
+        expect(pagination.find('.next').length).toBe(0);
+
+        expect(pagination.text()).toBe('前へ12345');
+        expect(pagination.find('.current').text()).toBe('55');
+    });
+});
