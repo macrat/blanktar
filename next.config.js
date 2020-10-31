1363f545c251ba73602cd42113d3332c5ed82463
MacRat <m@crat.jp>
2020-10-31 22:07:49 +0900

    Added new instagram CDN server

--- next.config.js
+++ next.config.js
@@ -130,6 +130,7 @@ module.exports = withBundleAnalyzer(withOffline(withMdxEnhanced({
             'scontent-sea1-1.cdninstagram.com',
             'scontent-sjc3-1.cdninstagram.com',
             'scontent-yyz1-1.cdninstagram.com',
+            'scontent-lax3-2.cdninstagram.com',
         ],
     },
     headers: () => [{
