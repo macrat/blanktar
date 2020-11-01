127fa851fc7552d608dc361295188189c63e913e
MacRat <m@crat.jp>
2020-11-01 14:53:06 +0900

    Remade tombo animation with transform only
    Closed #496

--- components/Tombo/index.tsx
+++ components/Tombo/index.tsx
@@ -1,37 +1,47 @@
 import React, { FC } from 'react';
 import { useAmp } from 'next/amp';
 
-import animation from './animation';
-
 
 const Tombo: FC = ({ children }) => (
     <div>
-        <svg width="16mm" height="16mm" viewBox="-0.5 -0.5 16 16" className={`tombo top left ${animation.className}`} aria-hidden="true">
-            <polyline points="15 0, 15 10, 5 10" fill="none" strokeWidth="0.2" className={animation.className} />
-            <polyline points="10 5, 10 15, 0 15" fill="none" strokeWidth="0.2" className={animation.className} />
+        <svg width="16mm" height="16mm" viewBox="-0.5 -0.5 16 16" className="top left" aria-hidden="true">
+            <g transform="translate(15,  0)"><line y2="10"                         className="vertical-early"  /></g>
+            <g transform="translate( 5, 10)"><line x2="10" transform-origin="10 0" className="horizontal-late" /></g>
+
+            <g transform="translate(10,  5)"><line y2="10"                         className="vertical-early"  /></g>
+            <g transform="translate( 0, 15)"><line x2="10" transform-origin="10 0" className="horizontal-late" /></g>
         </svg>
-        <svg width="16mm" height="16mm" viewBox="-0.5 -0.5 16 16" className={`tombo top center ${animation.className}`} aria-hidden="true">
-            <line x1="0" x2="15" y1="10" y2="10" strokeWidth="0.2" className={`horizontal ${animation.className}`} />
-            <line x1="7.5" x2="7.5" y1="5" y2="11" strokeWidth="0.2" className={`vertical ${animation.className}`} />
+        <svg width="16mm" height="16mm" viewBox="-0.5 -0.5 16 16" className="top center" aria-hidden="true">
+            <g transform="translate(  0, 10)"><line x2="15" className="horizontal-full" /></g>
+            <g transform="translate(7.5,  5)"><line y2=" 6" className="vertical-full"   /></g>
         </svg>
-        <svg width="16mm" height="16mm" viewBox="-0.5 -0.5 16 16" className={`tombo top right ${animation.className}`} aria-hidden="true">
-            <polyline points="0 0, 0 10, 10 10" fill="none" strokeWidth="0.2" className={animation.className} />
-            <polyline points="5 5, 5 15, 15 15" fill="none" strokeWidth="0.2" className={animation.className} />
+        <svg width="16mm" height="16mm" viewBox="-0.5 -0.5 16 16" className="top right" aria-hidden="true">
+            <g                             ><line y2="10" className="vertical-early"  /></g>
+            <g transform="translate(0, 10)"><line x2="10" className="horizontal-late" /></g>
+
+            <g transform="translate(5,  5)"><line y2="10" className="vertical-early"  /></g>
+            <g transform="translate(5, 15)"><line x2="10" className="horizontal-late" /></g>
         </svg>
 
         {children}
 
-        <svg width="16mm" height="16mm" viewBox="-0.5 -0.5 16 16" className={`tombo bottom left ${animation.className}`} aria-hidden="true">
-            <polyline points="15 15, 15 5, 5 5" fill="none" strokeWidth="0.2" className={animation.className} />
-            <polyline points="10 10, 10 0, 0 0" fill="none" strokeWidth="0.2" className={animation.className} />
+        <svg width="16mm" height="16mm" viewBox="-0.5 -0.5 16 16" className="bottom left" aria-hidden="true">
+            <g transform="translate(15, 5)"><line y2="10" transform-origin=" 0 10" className="vertical-early"  /></g>
+            <g transform="translate( 5, 5)"><line x2="10" transform-origin="10  0" className="horizontal-late" /></g>
+
+            <g transform="translate(10, 0)"><line y2="10" transform-origin=" 0 10" className="vertical-early"  /></g>
+            <g                             ><line x2="10" transform-origin="10  0" className="horizontal-late" /></g>
         </svg>
-        <svg width="16mm" height="16mm" viewBox="-0.5 -0.5 16 16" className={`tombo bottom center ${animation.className}`} aria-hidden="true">
-            <line x1="0" x2="15" y1="5" y2="5" strokeWidth="0.2" className={`horizontal ${animation.className}`} />
-            <line x1="7.5" x2="7.5" y1="4" y2="10" strokeWidth="0.2" className={`vertical ${animation.className}`} />
+        <svg width="16mm" height="16mm" viewBox="-0.5 -0.5 16 16" className="bottom center" aria-hidden="true">
+            <g transform="translate(  0, 5)"><line x2="15"                        className="horizontal-full" /></g>
+            <g transform="translate(7.5, 4)"><line y2=" 6" transform-origin="0 6" className="vertical-full"   /></g>
         </svg>
-        <svg width="16mm" height="16mm" viewBox="-0.5 -0.5 16 16" className={`tombo bottom right ${animation.className}`} aria-hidden="true">
-            <polyline points="0 15, 0 5, 10 5" fill="none" strokeWidth="0.2" className={animation.className} />
-            <polyline points="5 10, 5 0, 15 0" fill="none" strokeWidth="0.2" className={animation.className} />
+        <svg width="16mm" height="16mm" viewBox="-0.5 -0.5 16 16" className="bottom right" aria-hidden="true">
+            <g transform="translate(0, 5)"><line y2="10" transform-origin="0 10" className="vertical-early"  /></g>
+            <g transform="translate(0, 5)"><line x2="10"                         className="horizontal-late" /></g>
+
+            <g transform="translate(5, 0)"><line y2="10" transform-origin="0 10" className="vertical-early"  /></g>
+            <g transform="translate(5, 0)"><line x2="10"                         className="horizontal-late" /></g>
         </svg>
 
         <style jsx>{`
@@ -47,7 +57,7 @@ const Tombo: FC = ({ children }) => (
                 }
             }
 
-            .tombo {
+            svg {
                 position: absolute;
                 stroke: var(--colors-fg);
             }
@@ -72,13 +82,48 @@ const Tombo: FC = ({ children }) => (
                     width: 100%;
                     margin: 0;
                 }
-                .tombo {
+                svg {
                     display: none;
                 }
             }
-        `}</style>
 
-        {useAmp() ? null : animation.styles}
+            line {
+                stroke-width: 0.2;
+            }
+
+            @keyframes vertical-draw {
+                from { transform: scaleY(0); }
+                  to { transform: scaleY(1); }
+            }
+            @keyframes vertical-erase {
+                from { transform: scaleY(1); }
+                  to { transform: scaleY(0); }
+            }
+            @keyframes horizontal-draw {
+                from { transform: scaleX(0); }
+                  to { transform: scaleX(1); }
+            }
+            @keyframes horizontal-erase {
+                from { transform: scaleX(1); }
+                  to { transform: scaleX(0); }
+            }
+
+            .vertical-full   { animation: vertical-draw   .6s ease both; }
+            .horizontal-full { animation: horizontal-draw .6s ease both; }
+
+            .vertical-early   { animation: vertical-draw   .3s ease-in  both;     }
+            .vertical-late    { animation: vertical-draw   .3s ease-out both .3s; }
+            .horizontal-early { animation: horizontal-draw .3s ease-in  both;     }
+            .horizontal-late  { animation: horizontal-draw .3s ease-out both .3s; }
+
+            :global(main.loading) .vertical-full   { animation: vertical-erase   .6s ease both; }
+            :global(main.loading) .horizontal-full { animation: horizontal-erase .6s ease both; }
+
+            :global(main.loading) .vertical-early   { animation: vertical-erase   .3s ease-in  both .3s; }
+            :global(main.loading) .vertical-late    { animation: vertical-erase   .3s ease-out both;     }
+            :global(main.loading) .horizontal-early { animation: horizontal-erase .3s ease-in  both .3s; }
+            :global(main.loading) .horizontal-late  { animation: horizontal-erase .3s ease-out both;     }
+        `}</style>
     </div>
 );
 
